79e3d312f93a134c14db0045f10141fa8eeaf58e
th3j3ster <chris.carducci@gmail.com>
2014-11-26 08:08:51 -0500

    Update homeMonitor.py

--- homeMonitor.py
+++ homeMonitor.py
@@ -1,4 +1,5 @@
 import time
+
 import requests
 import json
 import cherrypy
@@ -14,13 +15,18 @@ def getTemp ():
     tempf = json.loads(requestTemp.text)
     theTemp = "{:.1f}".format(tempf["result"])
     return theTemp
-    #return str(tempf["result"])
+    # return str(tempf["result"])
     
 def getHum():
     requestHum = requests.get("https://api.spark.io/v1/devices/***REMOVED***/humidity?access_token=***REMOVED***")
     humidity = json.loads(requestHum.text)
     theHumidity = "{:.1f}".format(humidity["result"])
     return theHumidity
+
+def writeTemps():
+    f = open("temps.json", mode='w')
+    json.dump({'temp': getTemp(), 'hum': getHum()}, f, indent=4)
+    f.close()
 
 
 class TempPage(object):
@@ -46,4 +52,5 @@ if __name__ == '__main__':
     while True:
         getTemp()
         getHumidity()
+        writeTemps()
         time.sleep(5000)
